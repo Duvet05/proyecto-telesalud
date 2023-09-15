dd8e3647228330b9639e2c7594dd235e189b2e39
Duvet05 <gonzalo.galvezc@pucp.edu.pe>
2023-09-15 18:48:27 -0500

    GAAA

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from 'react';
 import './App.css';
+import MenuComponent from './components/MenuComponent'; // Asume que MenuComponent está en el mismo directorio que App.js
 
 function App() {
   return (
     <div className="App">
-
+      <MenuComponent />
     </div>
   );
 }
