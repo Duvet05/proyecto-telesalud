1e7ee1203c1e892a9eca222f1030e72bdc9672f4
Gonzalo <69012359+Duvet05@users.noreply.github.com>
2023-09-28 02:12:33 -0500

    INDEX

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,8 @@
 const assets = {
   images: {
     logo: require("./images/medical-logo-png-878.png"),
-    logo_alt: require("./images/medical-logo-png-878.png")
+    logo_alt: require("./images/medical-logo-png-878.png"),
+    doctor_picture: require("./images/portrait-of-a-happy-young-doctor-in-his-clinic-royalty-free-image-1661432441.jpg")
   }
 };
 
