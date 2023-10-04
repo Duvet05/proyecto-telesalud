842d4c66dc8cae28574081f5af931d04373b355e
Duvet05 <gonzalo.galvezc@pucp.edu.pe>
2023-10-04 14:59:31 -0500

    .

--- src/components/formNewAppointment/CompanionFields.jsx
+++ src/components/formNewAppointment/CompanionFields.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   TextField,
   Grid,
@@ -6,13 +6,23 @@ import {
   InputLabel,
   Select,
   MenuItem,
+  OutlinedInput,
+  FormHelperText,
 } from "@mui/material";
 
 function CompanionFields() {
   const [relationship, setRelationship] = React.useState("");
+  const [document, setDocument] = useState("");
+  const [documentError, setDocumentError] = useState(false);
 
   const handleRelationshipChange = (event) => {
     setRelationship(event.target.value);
+  };
+
+  const handleDocumentChange = (event) => {
+    const value = event.target.value;
+    setDocument(value);
+    setDocumentError(value.length !== 8);
   };
 
   const labels = [
@@ -26,7 +36,22 @@ function CompanionFields() {
     <Grid container spacing={3}>
       {labels.map((label, idx) => (
         <Grid item xs={6} key={idx}>
-          <TextField label={label} variant="outlined" required fullWidth />
+          <TextField
+            label={label}
+            variant="outlined"
+            required
+            fullWidth
+            // Solo permitir números en el campo 'DOCUMENTO DE IDENTIDAD'
+            inputProps={{
+              pattern: idx === 0 ? "[0-9]*" : undefined,
+              maxLength: idx === 0 ? 8 : undefined,
+            }}
+            error={idx === 0 && documentError}
+            helperText={
+              idx === 0 && documentError ? "Debe tener 8 dígitos" : ""
+            }
+            onChange={idx === 0 ? handleDocumentChange : undefined}
+          />
         </Grid>
       ))}
       <Grid item xs={6}>
@@ -41,6 +66,7 @@ function CompanionFields() {
             <MenuItem value={"hermana"}>Hermana</MenuItem>
             <MenuItem value={"madre"}>Madre</MenuItem>
             <MenuItem value={"padre"}>Padre</MenuItem>
+            <MenuItem value={"apoderado"}>Otros</MenuItem>
             <MenuItem value={"otros"}>Otros</MenuItem>
             {/* Puedes agregar más tipos de parentesco aquí */}
           </Select>
