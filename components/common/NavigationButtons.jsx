b595c39cef1c371bfa533b40842c257b698df001
Duvet05 <gonzalo.galvezc@pucp.edu.pe>
2023-10-06 16:22:24 -0500

    push

--- components/common/NavigationButtons.jsx
+++ components/common/NavigationButtons.jsx
@@ -3,31 +3,29 @@ import { ButtonGroup, Button, Box } from "@mui/material";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
-const NavigationButtons = ({ currentPage, totalPages, navigate }) => {
-  return (
-    <Box sx={{ mt: 2 }}>
-      <ButtonGroup variant="contained" color="primary" size="large" fullWidth>
-        <Button
-          startIcon={<ArrowBackIcon />}
-          disabled={currentPage === 0}
-          onClick={() => navigate(-1)}
-          variant={currentPage === 0 ? "outlined" : "contained"}
-          sx={{ transition: "all 0.3s" }}
-        >
-          Atrás
-        </Button>
-        <Button
-          endIcon={<ArrowForwardIcon />}
-          disabled={currentPage === totalPages - 1}
-          onClick={() => navigate(1)}
-          variant={currentPage === totalPages - 1 ? "outlined" : "contained"}
-          sx={{ transition: "all 0.3s" }}
-        >
-          Siguiente
-        </Button>
-      </ButtonGroup>
-    </Box>
-  );
-};
+const NavigationButtons = ({ currentPage, totalPages, navigate }) => (
+  <Box sx={{ mt: 2 }}>
+    <ButtonGroup variant="contained" color="primary" size="large" fullWidth>
+      <Button
+        startIcon={<ArrowBackIcon />}
+        disabled={currentPage === 0}
+        onClick={() => navigate(-1)}
+        variant={currentPage === 0 ? "outlined" : "contained"}
+        sx={{ transition: "all 0.3s" }}
+      >
+        Atrás
+      </Button>
+      <Button
+        endIcon={<ArrowForwardIcon />}
+        disabled={currentPage === totalPages - 1}
+        onClick={() => navigate(1)}
+        variant={currentPage === totalPages - 1 ? "outlined" : "contained"}
+        sx={{ transition: "all 0.3s" }}
+      >
+        Siguiente
+      </Button>
+    </ButtonGroup>
+  </Box>
+);
 
 export default NavigationButtons;
--- pages/NewAppointmentPage.js
+++ pages/NewAppointmentPage.js
@@ -1,48 +1,57 @@
+// Appointments.jsx
 import React, { useState, useEffect } from "react";
-import { Typography, Paper, Grid, Button, Container } from "@mui/material";
+import { Typography, Paper, Grid, Container } from "@mui/material";
 import AppointmentForm from "../components/appointments/AppointmentForm";
-import AskForTriage from "../components/appointments/AskForTriage";
 import AppointmentInfo from "./AppointmentInfo";
 import SelectMedic from "../components/appointments/SelectMedic";
 import NavigationButtons from "../components/common/NavigationButtons";
 import MainLayout from "@/components/layout/MainLayout";
-import TriajeONo from "../components/appointments/TriajeONo"
+import TriajeONo from "../components/appointments/TriageRequest";
+import CustomizedDialog from "@/components/appointments/CustomizedDialog";
+
+const CONFIRM_EXIT_MESSAGE =
+  "¿Está seguro de que desea abandonar esta página? Sus datos no guardados se perderán.";
+
+const PAGES = [
+  { component: <AppointmentForm />, title: "Información del paciente" },
+  { component: <SelectMedic />, title: "Seleccionar médico" },
+  { component: <TriajeONo />, title: "Triage" },
+  { component: <AppointmentInfo />, title: "Información de la cita" },
+];
+
 const Appointments = () => {
   const [currentPage, setCurrentPage] = useState(0);
-  const [isTriageOpen, setIsTriageOpen] = useState(false);
-
-  const PAGES = [<AppointmentForm />,<SelectMedic />, <TriajeONo /> ,<AppointmentInfo />];
-  const PAGE_TITLES = ["Información del paciente","Seleccionar médico"];
-
-  const openTriagePopup = () => setIsTriageOpen(true);
-  const closeTriagePopup = () => setIsTriageOpen(false);
+  const [isProcessCompleted, setIsProcessCompleted] = useState(false);
 
   const navigate = (delta) => {
-    const nextPage = Math.min(Math.max(currentPage + delta, 0), PAGES.length - 1);
+    const nextPage = Math.min(
+      Math.max(currentPage + delta, 0),
+      PAGES.length - 1
+    );
     setCurrentPage(nextPage);
 
     if (nextPage === PAGES.length - 1) {
-      openTriagePopup();
+      setIsProcessCompleted(true);
     }
   };
 
-  // useEffect(() => {
-  //   const confirmExit = (e) => {
-  //     e.preventDefault();
-  //     e.returnValue = "¿Está seguro de que desea abandonar esta página? Sus datos no guardados se perderán.";
-  //     return "";
-  //   };
+  useEffect(() => {
+    const confirmExit = (e) => {
+      e.preventDefault();
+      e.returnValue = CONFIRM_EXIT_MESSAGE;
+      return "";
+    };
 
-  //   window.addEventListener("beforeunload", confirmExit);
+    window.addEventListener("beforeunload", confirmExit);
 
-  //   return () => {
-  //     window.removeEventListener("beforeunload", confirmExit);
-  //   };
-  // }, []);
+    return () => {
+      window.removeEventListener("beforeunload", confirmExit);
+    };
+  }, []);
 
   return (
     <MainLayout>
-      <Container maxWidth={false} style={{height: "auto"}}>
+      <Container maxWidth={false} style={{ height: "auto" }}>
         <Paper elevation={3} style={{ padding: "20px", marginTop: "20px" }}>
           <Typography variant="h4" gutterBottom>
             Nueva Atención
@@ -51,26 +60,26 @@ const Appointments = () => {
           <Grid>
             <Grid item xs={12}>
               <Typography variant="h5" gutterBottom>
-                {PAGE_TITLES[currentPage]}
+                {PAGES[currentPage].title}
               </Typography>
-              {PAGES[currentPage]}
+              {PAGES[currentPage].component}
             </Grid>
-            { (
-              <Grid item xs={12}>
-                <NavigationButtons
-                  currentPage={currentPage}
-                  totalPages={PAGES.length}
-                  navigate={navigate}
-                />
-              </Grid>
-            )}
+            <Grid item xs={12}>
+              <NavigationButtons
+                currentPage={currentPage}
+                totalPages={PAGES.length}
+                navigate={navigate}
+              />
+            </Grid>
           </Grid>
-          {/* <AskForTriage
-          open={isTriageOpen}
-          onClose={() => setIsTriageOpen(false)}
-        /> */}
         </Paper>
       </Container>
+      {isProcessCompleted && (
+        <CustomizedDialog
+          onClose={() => setIsProcessCompleted(false)}
+          open={isProcessCompleted}
+        />
+      )}
     </MainLayout>
   );
 };
