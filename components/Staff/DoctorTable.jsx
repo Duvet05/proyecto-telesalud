987fe074a980f59411c8bb0b1cfc3b510a526817
Duvet05 <gonzalo.galvezc@pucp.edu.pe>
2023-10-14 02:51:08 -0500

    WORKING WITH BASE TABLE

--- components/Staff/DoctorTable.jsx
+++ components/Staff/DoctorTable.jsx
@@ -1,137 +1,46 @@
-import React, { useState, useEffect } from "react";
-import {
-  TableContainer,
-  Table,
-  TableHead,
-  TableBody,
-  TableRow,
-  TableCell,
-  Paper,
-  Button,
-  TableSortLabel,
-} from "@mui/material";
+import React from "react";
+import { Button, TableCell, TableRow } from "@mui/material";
 import Link from "next/link";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import { medicService } from "../../services/medicService";
+import BaseTable from "../common/BaseTable";
 
-export const DoctorTable = (props) => {
-  const { doctorNameIngresado, especialidad } = props;
-  const [isLoading, setIsLoading] = useState(true);
-  const [doctors, setDoctors] = useState([]);
-  const [order, setOrder] = useState("asc");
-  const [orderBy, setOrderBy] = useState("nombre");
+// Function to fetch doctors
+const fetchDoctors = async ({ doctorNameIngresado, especialidad }) => {
+  const data = await medicService.buscarPorNombre("");
+  return data.filter((doctor) => {
+    const nombreCompleto = `${doctor.nombres} ${doctor.apellidoPaterno} ${doctor.apellidoMaterno}`;
+    const nombreCompletoSinAcentos =
+      removeAccents(nombreCompleto).toLowerCase();
+    const doctorNameIngresadoSinAcentos =
+      removeAccents(doctorNameIngresado).toLowerCase();
 
-  useEffect(() => {
-    fetchDoctors();
-  }, [especialidad, doctorNameIngresado]);
-
-  const fetchDoctors = async () => {
-    try {
-      const data = await medicService.buscarPorNombre("");
-      const doctoresFiltrados = data.filter((doctor) => {
-        const nombreCompleto = `${doctor.nombres} ${doctor.apellidoPaterno} ${doctor.apellidoMaterno}`;
-        const nombreCompletoSinAcentos =
-          removeAccents(nombreCompleto).toLowerCase();
-        const doctorNameIngresadoSinAcentos =
-          removeAccents(doctorNameIngresado).toLowerCase();
-
-        if (especialidad === "todasLasEspecialidades") {
-          return (
-            nombreCompletoSinAcentos.includes(doctorNameIngresadoSinAcentos) ||
-            doctor.dni.includes(doctorNameIngresadoSinAcentos)
-          );
-        } else {
-          return (
-            doctor.especialidad.idEspecialidad === especialidad &&
-            (nombreCompletoSinAcentos.includes(doctorNameIngresadoSinAcentos) ||
-              doctor.dni.includes(doctorNameIngresadoSinAcentos))
-          );
-        }
-      });
-
-      setDoctors(doctoresFiltrados);
-      setIsLoading(false);
-    } catch (error) {
-      console.error("Failed to fetch doctors:", error);
-      setIsLoading(false);
+    if (especialidad === "todasLasEspecialidades") {
+      return (
+        nombreCompletoSinAcentos.includes(doctorNameIngresadoSinAcentos) ||
+        doctor.dni.includes(doctorNameIngresadoSinAcentos)
+      );
+    } else {
+      return (
+        doctor.especialidad.idEspecialidad === especialidad &&
+        (nombreCompletoSinAcentos.includes(doctorNameIngresadoSinAcentos) ||
+          doctor.dni.includes(doctorNameIngresadoSinAcentos))
+      );
     }
-  };
-
-  const handleSortRequest = (property) => {
-    const isAsc = orderBy === property && order === "asc";
-    setOrder(isAsc ? "desc" : "asc");
-    setOrderBy(property);
-  };
-
-  const sortedDoctors = getSortedDoctors(doctors, orderBy, order);
-
-  return (
-    !isLoading && (
-      <TableContainer component={Paper}>
-        <Table aria-label="doctor table">
-          <DoctorTableHead
-            handleSortRequest={handleSortRequest}
-            order={order}
-            orderBy={orderBy}
-          />
-          <DoctorTableBody doctors={sortedDoctors} />
-        </Table>
-      </TableContainer>
-    )
-  );
+  });
 };
 
-const DoctorTableHead = ({ handleSortRequest, order, orderBy }) => (
-  <TableHead>
-    <TableRow>
-      {["Nombre Completo", "DNI", "Código Médico", "Área", "Especialidad"].map(
-        (header, index) => (
-          <TableCell
-            key={index}
-            sx={{
-              fontSize: "1.1em",
-              color: "#333",
-              paddingBottom: "10px",
-              paddingTop: "10px",
-            }}
-          >
-            {header === "DNI" ? (
-              <TableSortLabel
-                active={orderBy === "dni"}
-                direction={order}
-                onClick={() => handleSortRequest("dni")}
-              >
-                {header}
-              </TableSortLabel>
-            ) : (
-              header
-            )}
-          </TableCell>
-        )
-      )}
-    </TableRow>
-  </TableHead>
-);
-
-const DoctorTableBody = ({ doctors }) => (
-  <TableBody>
-    {doctors.map((doctor) => (
-      <DoctorRow key={doctor.idDoctor} doctor={doctor} />
-    ))}
-  </TableBody>
-);
-
-const DoctorRow = ({ doctor }) => (
+const DoctorRowComponent = ({ data }) => (
   <TableRow sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
     <TableCell>
-      {`${doctor.apellidoPaterno.toUpperCase()} ${doctor.apellidoMaterno.toUpperCase()}, ${doctor.nombres.toUpperCase()}`}
+      {`${data.apellidoPaterno.toUpperCase()} ${data.apellidoMaterno.toUpperCase()}, ${data.nombres.toUpperCase()}`}
     </TableCell>
-    <TableCell>{doctor.dni}</TableCell>
-    <TableCell>{doctor.cmp}</TableCell>
-    <TableCell>{doctor.area.toUpperCase()}</TableCell>
-    <TableCell>{doctor.especialidad.nombre.toUpperCase()}</TableCell>
+    <TableCell>{data.dni}</TableCell>
+    <TableCell>{data.cmp}</TableCell>
+    <TableCell>{data.area.toUpperCase()}</TableCell>
+    <TableCell>{data.especialidad.nombre.toUpperCase()}</TableCell>
     <TableCell>
-      <Link href={`/staff/perfil/${doctor.idPersona}`} passHref>
+      <Link href={`/staff/perfil/${data.idPersona}`} passHref>
         <Button
           variant="contained"
           sx={{
@@ -149,22 +58,26 @@ const DoctorRow = ({ doctor }) => (
   </TableRow>
 );
 
-const removeAccents = (str) =>
-  str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+const DoctorTable = (props) => {
+  const columns = [
+    { id: "nombreCompleto", label: "Nombre Completo", sortable: false },
+    { id: "dni", label: "DNI", sortable: true },
+    { id: "codigoMedico", label: "Código Médico", sortable: false },
+    { id: "area", label: "Área", sortable: false },
+    { id: "especialidad", label: "Especialidad", sortable: false },
+  ];
 
-const getSortedDoctors = (doctors, orderBy, order) => {
-  const comparator = (a, b) => {
-    switch (orderBy) {
-      case "dni":
-        return parseInt(a.dni, 10) - parseInt(b.dni, 10);
-      default:
-        return 0;
-    }
-  };
-
-  return [...doctors].sort(
-    (a, b) => (order === "desc" ? -1 : 1) * comparator(a, b)
+  return (
+    <BaseTable
+      fetchData={fetchDoctors}
+      columns={columns}
+      RowComponent={DoctorRowComponent}
+      extraProps={props}
+    />
   );
 };
+
+const removeAccents = (str) =>
+  str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
 
 export default DoctorTable;
