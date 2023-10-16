18350aaad129b92cb346eac6ca1d191ff0a91012
Duvet05 <gonzalo.galvezc@pucp.edu.pe>
2023-10-16 12:40:06 -0500

    temporary workaround appointment tables

--- components/common/SearchAndAddBar.jsx
+++ components/common/SearchAndAddBar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {
   TextField,
+  Paper,
   Button,
   InputAdornment,
   IconButton,
@@ -10,45 +11,83 @@ import Link from "next/link";
 import SearchIcon from "@mui/icons-material/Search";
 import AddIcon from "@mui/icons-material/Add";
 
-const SearchAndAddBar = ({ searchTerm, onSearchChange }) => (
-  <Grid container spacing={2} alignItems="center">
-    <Grid item xs={12} md={6}>
-      <TextField
-        fullWidth
-        variant="outlined"
-        placeholder="Buscar por nombre, email, especialidad..."
-        value={searchTerm}
-        onChange={onSearchChange}
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <IconButton>
-                <SearchIcon />
-              </IconButton>
-            </InputAdornment>
-          ),
-        }}
-      />
-    </Grid>
-    <Grid item xs={12} md={6} container justifyContent="flex-end">
-      <Link href="/NewAppointmentPage">
+const SearchAndAddBar = ({
+  searchTerm,
+  onSearchChange,
+  linkHref = "/NewAppointmentPage",
+}) => (
+  <Paper sx={{ my: 2, p: 2 }}>
+    <Grid
+      container
+      spacing={1}
+      alignItems="center"
+      justifyContent="space-between"
+    >
+      {/* Search Input */}
+      <Grid item xs={2} md={7}>
+        <TextField
+          fullWidth
+          variant="outlined"
+          placeholder="Buscar por nombre, email, especialidad..."
+          value={searchTerm}
+          onChange={onSearchChange}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <IconButton>
+                  <SearchIcon />
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Grid>
+
+      {/* Search Button */}
+      <Grid item xs={12} md={2}>
         <Button
-          variant="contained"
+          fullWidth
           sx={{
             backgroundColor: "#2196f3",
-            color: "white",
+            color: "#ffffff",
+            fontSize: "1.0em",
             textTransform: "none",
             "&:hover": {
               backgroundColor: "#b3b3b3",
             },
+            "& .MuiButton-startIcon": {
+              margin: 0,
+              marginRight: "4px",
+            },
           }}
-          startIcon={<AddIcon />}
+          startIcon={<SearchIcon />}
         >
-          Nueva cita
+          Buscar
         </Button>
-      </Link>
+      </Grid>
+
+      {/* Add Button */}
+      <Grid item xs={12} md={2}>
+        <Link href={linkHref}>
+          <Button
+            fullWidth
+            variant="contained"
+            sx={{
+              backgroundColor: "#2196f3",
+              color: "white",
+              textTransform: "none",
+              "&:hover": {
+                backgroundColor: "#b3b3b3",
+              },
+            }}
+            startIcon={<AddIcon />}
+          >
+            Nueva cita
+          </Button>
+        </Link>
+      </Grid>
     </Grid>
-  </Grid>
+  </Paper>
 );
 
 export default SearchAndAddBar;
