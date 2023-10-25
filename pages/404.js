d183855ec4e6a37331daa99dab8027379d3e836a
Duvet05 <gonzalo.galvezc@pucp.edu.pe>
2023-10-25 16:10:34 -0500

    eliminacion elementos innecesarias

--- pages/404.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import PageNotFound from "@/components/common/PageNotFound/PageNotFound";
-
-const Custom404 = () => {
-  return <PageNotFound />;
-};
-
-export default Custom404;
