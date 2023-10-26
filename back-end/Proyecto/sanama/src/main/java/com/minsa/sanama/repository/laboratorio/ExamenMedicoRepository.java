56ebf6aa832ca269dac45b20156df2cdc2d95336
zaicket <vargas.pedro@pucp.edu.pe>
2023-10-26 12:52:32 -0500

    (hecho por sergio) listar y registrar examen medico

--- back-end/Proyecto/sanama/src/main/java/com/minsa/sanama/repository/laboratorio/ExamenMedicoRepository.java
+++ back-end/Proyecto/sanama/src/main/java/com/minsa/sanama/repository/laboratorio/ExamenMedicoRepository.java
@@ -1,6 +1,9 @@
 package com.minsa.sanama.repository.laboratorio;
 
+import com.minsa.sanama.model.admision.Paciente;
+import com.minsa.sanama.model.atencionmedica.CitaMedica;
 import com.minsa.sanama.model.laboratorio.ExamenMedico;
+import com.minsa.sanama.model.laboratorio.OrdenLaboratorio;
 import com.minsa.sanama.model.rrhh.Especialidad;
 import com.minsa.sanama.model.rrhh.Medico;
 import com.minsa.sanama.model.rrhh.TurnoAtencion;
@@ -22,6 +25,14 @@ import java.util.Map;
 public class ExamenMedicoRepository {
     @Autowired
     JdbcTemplate jdbcTemplate;
+    public ExamenMedicoRepository(JdbcTemplate jdbcTemplate) { this.jdbcTemplate = jdbcTemplate;}
+
+    private final ExamenMedicoRepository.ExamenMedicoMapper examenMedicoMapper = new ExamenMedicoRepository.ExamenMedicoMapper();
+
+    public List<ExamenMedico> listarExamenMedicoID(String pv_filtro) {
+        String procedureCall = "{call dbSanama.ssm_lab_listar_examen_medico('"+pv_filtro+"')};";
+        return jdbcTemplate.query(procedureCall, examenMedicoMapper);
+    }
 
     public int registrarExamenMedico(ExamenMedico examenMedico) {
         SimpleJdbcCall simpleJdbcCall = new SimpleJdbcCall(jdbcTemplate)
@@ -53,6 +64,43 @@ public class ExamenMedicoRepository {
         } else {
             int idExamenMedico = (int) result.get("pn_id_examen");
             return idExamenMedico;
+        }
+    }
+
+    private static class ExamenMedicoMapper implements RowMapper<ExamenMedico> {
+        @Override
+        public ExamenMedico mapRow(ResultSet rs, int rowNum) throws SQLException {
+
+            ExamenMedico examen = new ExamenMedico();
+
+            Paciente paciente = new Paciente();
+            paciente.setDni(rs.getString("dni_paciente"));
+            paciente.setNombres(rs.getString("nombres_paciente"));
+            paciente.setApellidoPaterno(rs.getString("apellido_paterno_paciente"));
+            paciente.setApellidoMaterno(rs.getString("apellido_materno_paciente"));
+            paciente.setDni(rs.getString("dni"));
+
+            Medico medico = new Medico();
+            medico.setNombres(rs.getString("nombres_medico"));
+            medico.setApellidoPaterno(rs.getString("apellido_paterno_medico"));
+            medico.setApellidoMaterno(rs.getString("apellido_materno_medico"));
+
+            examen.setIdExamen(rs.getInt("id_examen"));
+            OrdenLaboratorio orden = new OrdenLaboratorio();
+            CitaMedica cm = new CitaMedica();
+            cm.setMedico(medico);
+            cm.setPaciente(paciente);
+            cm.setIdCita(rs.getInt("id_cita"));
+            orden.setCitaMedica(cm);
+            orden.setIdOrdenLaboratorio(rs.getInt("id_orden_laboratorio"));
+            examen.setOrdenLaboratorio(orden);
+            examen.setNombre(rs.getString("nombre_doctor_firmante"));
+            examen.setTipo(rs.getString("tipo"));
+            examen.setObservaciones(rs.getString("observaciones"));
+            examen.setArchivo(rs.getBytes("archivo"));
+            examen.setEstado(rs.getInt("estado"));
+
+            return examen;
         }
     }
 }
--- back-end/Proyecto/sanama/src/main/java/com/minsa/sanama/services/laboratorio/ExamenMedicoService.java
+++ back-end/Proyecto/sanama/src/main/java/com/minsa/sanama/services/laboratorio/ExamenMedicoService.java
@@ -1,4 +1,32 @@
 package com.minsa.sanama.services.laboratorio;
 
+import com.minsa.sanama.model.laboratorio.ExamenMedico;
+import com.minsa.sanama.model.laboratorio.OrdenLaboratorio;
+import com.minsa.sanama.model.rrhh.Medico;
+import com.minsa.sanama.model.rrhh.TurnoAtencion;
+import com.minsa.sanama.repository.laboratorio.ExamenMedicoRepository;
+import com.minsa.sanama.repository.rrhh.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
 public class ExamenMedicoService {
+
+    @Autowired
+    ExamenMedicoRepository examenMedicoRepository;
+
+    public int registrarExamenMedico(ExamenMedico examenMedico) {
+        int idExamenMedico;
+        idExamenMedico = examenMedicoRepository.registrarExamenMedico(examenMedico);
+        return idExamenMedico;
+    }
+
+    public List<ExamenMedico> listarExamenMedico(String pv_filtro) {
+        List<ExamenMedico> lexamenes;
+        lexamenes = examenMedicoRepository.listarExamenMedicoID(pv_filtro);
+        return lexamenes;
+    }
 }
